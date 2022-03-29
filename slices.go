5173354b4b4df98f3a27fdd7c94272ce87450d86
Alexander Wong <alexandthemachine@gmail.com>
2022-03-29 17:06:12 +0800

    tried removing an entry by appending 2 diff slices surrounding the given item

--- slices.go
+++ slices.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+var spending = []float64{9.50, 8.00, 10.20, 7.40, 100.00}
+
 func main() {
-	//Insert Code here
+
+	activity1()
+	spending = append(spending, 7.0)
+	activity3()
+
+	// a bit of activity 4 + removing an item from the slice?
+	// removing entry 3
+	set1 := spending[0:2]
+	set2 := spending[3:5]
+	for _, value := range set2 {
+		set1 = append(set1, value)
+	}
+	spending = set1
+	fmt.Println(spending)
+}
+
+func activity1() {
+	fmt.Println(spending)
+	fmt.Println(len(spending))
+	fmt.Println(cap(spending))
+}
+
+func activity3() {
+	spending[2] = 9.8
+	fmt.Println(spending)
 }
